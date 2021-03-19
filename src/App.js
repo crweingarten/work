27b5ee71e2e35b8ca5487cb622f0b022699953c2
crweingarten <65681769+crweingarten@users.noreply.github.com>
2021-03-19 13:26:57 -0700

    without BrowserRouter

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Contact from './pages/Contact'
 import Work from './pages/Work'
 import Fade from 'react-reveal/Fade'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {BrowserRouter} from "react-router-dom"
+// import {BrowserRouter} from "react-router-dom"
 
 
 
@@ -14,12 +14,12 @@ import {BrowserRouter} from "react-router-dom"
 function App() {
 
   return (
-    <BrowserRouter>
+    <div>
     <Name/>
     <Fade bottom><div className="home-ital-sm pad" align="center"><span>↓</span></div></Fade>
     <Fade bottom><Work /></Fade>
     <Contact/>
-    </BrowserRouter>  );
+    </div>  );
 }
 
 
